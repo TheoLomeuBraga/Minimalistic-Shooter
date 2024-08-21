d01900e56efe3e4c212ffc95986c14484580d7ec
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2024-08-21 09:47:51 -0300

    a

--- assets/NPCs/test enemy/test_enemy.gd
+++ assets/NPCs/test enemy/test_enemy.gd
@@ -2,5 +2,10 @@ extends GenericPathFinder
 
 var health : int = 10
 
+func _ready() -> void:
+	look_mode = 2
+
 func movement_plugin(delta: float) -> void:
+	target_location = Global.player.global_position
 	go = global_position.distance_to(Global.player.global_position) > 5
+	
