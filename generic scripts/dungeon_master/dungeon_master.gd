006f780b6090c1991936870e0a12693cadc37565
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2024-08-24 09:53:57 -0300

    a

--- generic scripts/dungeon_master/dungeon_master.gd
+++ generic scripts/dungeon_master/dungeon_master.gd
@@ -5,6 +5,7 @@ var spawners_list : Array[Node3D]
 
 @export var wave_number : int = 0
 @export var wave_dificulty : float = 0.0
+@export var wave_max_dificulty : float = 10.0
 @export var wave_duration : float = 0.0
 var cooldown : float = 0.0
 
@@ -23,14 +24,31 @@ func select_random_non_visible_node():
 	return null
 	
 
+func start_wave() -> void:
+	wave_dificulty += 1
+	if wave_dificulty > wave_max_dificulty:
+		wave_dificulty = wave_max_dificulty
+	wave_duration = 30.0
+
+func spawn_enemy(position : Vector3):
+	var selected_enemy : int = 0
+	var enemy_instance : Node3D = enemy_list[selected_enemy].instantiate()
+	enemy_instance.global_position = position
+	get_tree().get_root().add_child(enemy_instance)
+	
+
+func update_wave(delta: float) -> void:
+	if wave_duration > 0:
+		var random_spawn : Node3D = select_random_non_visible_node()
+		if random_spawn != null and cooldown <= 0:
+			spawn_enemy(random_spawn.global_position)
+			cooldown = min(max(wave_max_dificulty - wave_dificulty,1),3)
+			
+		wave_duration -= delta
+		cooldown -= delta
+
+func _ready() -> void:
+	start_wave()
+
 func _process(delta: float) -> void:
-	
-	var random_spawn : Node3D = select_random_non_visible_node()
-	if random_spawn != null and cooldown <= 0:
-		var enemy_instance : Node3D = enemy_list[0].instantiate()
-		enemy_instance.global_position = random_spawn.global_position
-		get_tree().get_root().add_child(enemy_instance)
-		cooldown = 1.0
-	
-	wave_duration -= delta
-	cooldown -= delta
+	update_wave(delta)
