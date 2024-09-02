fce6f5dee004421df03050c00a46257365c9f229
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2024-09-02 08:37:20 -0300

    a

--- assets/guns/test gun/test_gun.gd
+++ assets/guns/test gun/test_gun.gd
@@ -5,10 +5,12 @@ extends Node3D
 func _ready() -> void:
 	pass
 
-@export var bullet_asset : PackedScene
+@export var burst_fire_count : int = 3
+@export var range : float = 25.0
+@export var damage : int = 5
 
 var burst_fire_timer : float = 0.0
-@export var burst_fire_count : int = 3
+
 
 var time_between_shots : float = 0.25 / float(burst_fire_count)
 var burst_firre_cooldown : float = 0.0
@@ -18,6 +20,7 @@ var cooldown : float = 0.0
 var rng : RandomNumberGenerator = RandomNumberGenerator.new()
 
 func shoot(delta: float,inaccuracy : float = 0.0):
+	'''
 	if inaccuracy > 0:
 		var bullet : Node3D = bullet_asset.instantiate()
 		$muzle.look_at(Global.player.target)
@@ -36,7 +39,38 @@ func shoot(delta: float,inaccuracy : float = 0.0):
 		bullet.position -= bullet.basis.z.normalized() * (delta + 1)
 		bullet.set_distance(delta * bullet.speed)
 		get_tree().get_root().add_child(bullet)
+	'''
+	
+	var player_raycast : RayCast3D = Global.player.get_node("Camera3D/RayCast3D")
+	
+	var saved_rotation_raycast : Vector3 = player_raycast.rotation
+	var saved_target_position_raycast : Vector3 = player_raycast.target_position
+	
+	player_raycast.target_position.z = -range
+	if inaccuracy > 0:
 		
+		player_raycast.rotation_degrees.x += rng.randf_range(-inaccuracy,inaccuracy)
+		player_raycast.rotation_degrees.y += rng.randf_range(-inaccuracy,inaccuracy)
+		print(player_raycast.rotation_degrees)
+		
+		player_raycast.force_update_transform()
+		player_raycast.force_raycast_update()
+		if player_raycast.is_colliding():
+			var colider : Node3D = player_raycast.get_collider()
+			if colider.has_method("hit_damage"):
+				colider.hit_damage(damage)
+	else:
+		player_raycast.force_update_transform()
+		player_raycast.force_raycast_update()
+		if player_raycast.is_colliding():
+			var colider : Node3D = player_raycast.get_collider()
+			if colider.has_method("hit_damage"):
+				colider.hit_damage(damage)
+	
+	player_raycast.rotation = saved_rotation_raycast
+	player_raycast.target_position = saved_target_position_raycast
+	player_raycast.force_update_transform()
+	player_raycast.force_raycast_update()
 
 enum fire_modes {
 	semi_auto = 0,
@@ -69,14 +103,14 @@ func _physics_process(delta: float) -> void:
 			
 			burst_firre_cooldown -= delta
 			if burst_firre_cooldown <= 0:
-				shoot(delta)
+				shoot(delta,2)
 				Global.player.shot_animation()
 				cooldown = 0.5
 				burst_firre_cooldown = time_between_shots
 				
 	elif fire_mode == 2:
 		if Input.is_action_pressed("fire") and not Global.is_paused and cooldown < 0:
-			shoot(delta)
+			shoot(delta,5)
 			Global.player.shot_animation()
 			cooldown = 0.1
 		
