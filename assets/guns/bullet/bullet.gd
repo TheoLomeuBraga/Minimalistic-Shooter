85405be84b9c7e620b76debad0851587a41fdf32
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2024-09-02 18:42:49 -0300

    a

--- assets/guns/bullet/bullet.gd
+++ assets/guns/bullet/bullet.gd
@@ -1,8 +1,10 @@
-extends Sprite3D
+extends Node3D
 
 func set_distance(distance : float):
 	$RayCast3D.position.z = -distance * 2
 	$RayCast3D.target_position.z = distance * 2
+
+
 
 func _ready() -> void:
 	set_distance(0)
@@ -15,8 +17,16 @@ func _ready() -> void:
 
 var first_frame = true
 
+var on_frame : int = 0
+func _process(delta: float) -> void:
+	if on_frame > 1:
+		visible = true
+	else:
+		on_frame += 1
+
 func _physics_process(delta: float) -> void:
 	
+	$sprite.position = $sprite.position.lerp(Vector3.ZERO, speed * delta)
 	
 	var obj_coliding : Node3D = $RayCast3D.get_collider()
 	if $RayCast3D.is_colliding():
@@ -25,10 +35,12 @@ func _physics_process(delta: float) -> void:
 			
 			if obj_coliding != null and obj_coliding.has_method("hit_damage"):
 				obj_coliding.hit_damage(damage)
-			queue_free()
+				queue_free()
 	
 	if distance < 0:
 		queue_free()
+	
+	
 	
 	if first_frame:
 		first_frame = false
